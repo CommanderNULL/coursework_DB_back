c3a91884ae2e63bed723236ecb01111259ba0de2
commander_null <molodevgenij@yandex.ru>
2018-04-09 18:54:34 +0300

    Added sql file an changed query

---
+++ coursework.sql
@@ -0,0 +1,67 @@
+--CREATE TABLE HUMANS (
+--    id_human integer,
+--    dob date,
+--    name varchar(30),
+--    surname varchar(30),
+--    PRIMARY KEY (id_human)
+--)
+
+--CREATE TABLE CATEGORIES (
+--        id_category integer,
+--        description varchar(100),
+--        PRIMARY KEY (id_category)
+--)
+-- 
+-- CREATE TABLE TAG_DESCR (
+--        id_tag_type integer,
+--        tag_descr varchar(30),
+--        PRIMARY KEY (id_tag_type)
+-- )
+
+--CREATE TABLE FILMS (
+--    id_film integer,
+--    title varchar(30),
+--    release_date date,
+--    PRIMARY KEY (id_film)
+--
+
+--CREATE TABLE OSCARS (
+--    id_oscar integer not null,
+--    PRIMARY KEY (id_oscar)
+--)
+
+--CREATE TABLE ACTORS(
+--    id_actor integer not null,
+--    id_human integer not null,
+--    id_film integer not null,
+--    role varchar(30) not null,
+--    PRIMARY KEY (id_actor),
+--    FOREIGN KEY (id_human) REFERENCES humans (id_human)
+--)
+
+--CREATE TABLE TAGS (
+--    id_tag integer not null,
+--    id_tag_type integer not null,
+--    id_film integer not null,
+--    PRIMARY KEY (id_tag),
+--    FOREIGN KEY (id_tag_type) REFERENCES tag_descr (id_tag_type),
+--    FOREIGN KEY (id_film) REFERENCES films (id_film)
+--)
+
+--CREATE TABLE reviews (
+--    id_review integer not null,
+--    id_film integer not null,
+--    id_human integer not null,
+--    review_text varchar(1000),
+--    mark integer not null,
+--    PRIMARY KEY (id_review),
+--    FOREIGN KEY (id_film) REFERENCES films (id_film),
+--    FOREIGN KEY (id_human) REFERENCES humans (id_human)
+--)
+
+--select f.TITLE, cat.description, rev.id_human, rev.MARK
+--from films f, oscars osc, reviews rev, categories cat
+--where
+--f.id_film = osc.id_film
+--AND rev.id_film = f.id_film
+--AND cat.id_category = osc.ID_CATEGORY;
--- database_ex.js
+++ database_ex.js
@@ -4,7 +4,7 @@ const get_result = function () {
   oracledb.outFormat = oracledb.OBJECT;
   oracledb.getConnection(
     {
-      user          : "STUD",
+      user          : "SYSTEM",
       password      : "x",
       connectString : "localhost/XE"
     },
@@ -13,8 +13,8 @@ const get_result = function () {
       if (err) { console.error(err); return; }
       connection.execute(
         "SELECT * "
-      + "FROM employees "
-      + "WHERE id_e<100 AND rownum < 3",
+      + "FROM humans "
+      + "WHERE rownum < 3",
         function(err, result)
         {
           if (err) { console.error(err); return; }
